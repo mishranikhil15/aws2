dea909bb1ef4c1cb243631c661d17f1e0fc47737
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-03-20 00:44:22 +0530

    aws2

---
+++ index.js
@@ -0,0 +1,12 @@
+const express= require("express");
+
+const app= express();
+
+
+app.get("/search",(req,res)=>{
+    res.send("welcome to my server2");
+})
+
+app.listen(4100,async()=>{
+   console.log(`server is running on port ${4100}`)
+})
